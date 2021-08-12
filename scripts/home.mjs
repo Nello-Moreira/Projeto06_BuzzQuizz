315f6ebc616ae8e6a845c15ac33487b00613f962
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-08-11 21:33:41 -0300

    Added function to get quizzes from server and render them on screen

--- scripts/home.mjs
+++ scripts/home.mjs
@@ -1,1 +1,33 @@
+const axiosBase = axios.create({
+    baseURL: 'https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes',
+});
+let serverQuizzesElement = document.querySelector('.server-quizzes .quizzes-list');
+let userQuizzesElement = document.querySelector('.user-quizzes .quizzes-list');
+serverQuizzesElement.innerHTML = '';
+
+getServerQuizzes();
+
+function getServerQuizzes(){
+    let promise = axiosBase.get();
+    promise.then(renderServerQuizzes); //futuramente fazer uma função para filtrar entre os quizzes do servidor, aqueles criados pelo usuário
+}
+
+function renderServerQuizzes(quizzes){
+    console.log('oi');
+    for (let i = 0; i < quizzes.data.length; i++) {
+        console.log(quizzes.data[i].image);
+        serverQuizzesElement.innerHTML += `
+            <li>
+                <img src="${quizzes.data[i].image}" alt="">
+                <h4>${quizzes.data[i].title}</h4>
+            </li>
+        `
+        
+    }
+}
+
+
+
+
+
 export {  };
