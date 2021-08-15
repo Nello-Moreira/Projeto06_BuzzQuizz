cc430c7897e93eeef3741afb60afb4c6bcba5471
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-08-14 23:16:46 -0300

    fixed duplicated exports

--- scripts/overall.mjs
+++ scripts/overall.mjs
@@ -2,8 +2,6 @@ import { startHomeClickEvents, getServerQuizzes } from './home.mjs';
 import { startQuizzClickEvents } from './quizz_questions.mjs';
 import { activeTriggerEvents, removeTriggerEvents } from './quizz_creation.mjs';
 
-export { axiosBase, scrollToHeader };
-
 const axiosBase = axios.create({
     baseURL: 'https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes',
 });
@@ -27,4 +25,4 @@ startQuizzClickEvents();
 activeTriggerEvents();
 
 
-export {scrollToHeader, hideLoader};
+export {scrollToHeader, hideLoader, axiosBase};
