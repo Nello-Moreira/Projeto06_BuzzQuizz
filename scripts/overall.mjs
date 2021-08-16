1c927632c59d34ede7976329ca09b1778e2b24a8
Nello-Moreira <g.moreira1@outlook.com>
2021-08-16 14:42:59 -0300

    changed api version

--- scripts/overall.mjs
+++ scripts/overall.mjs
@@ -8,7 +8,7 @@ let quizzPageElement = document.querySelector('#quizz');
 let creationPageELement = document.querySelector('#quizz-creation');
 
 const axiosBase = axios.create({
-    baseURL: 'https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes',
+    baseURL: 'https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes',
 });
 
 function scrollToHeader() {
